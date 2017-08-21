f08ee9e24fca9cdcef7b5a9afb1029b91a94b17d
house1998 <house1998@naver.com>
2017-08-21 15:05:16 +0900

    edittext radio 다이얼로그 추가 및 FulladdressActivity 추가

---
+++ app/src/main/java/com/a2017hkt15/sortaddr/FullAddressActivty.java
@@ -0,0 +1,13 @@
+package com.a2017hkt15.sortaddr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullAddressActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_address_activty);
+    }
+}
