67bdf1f020edc887ced094c358be617abe9f97ba
YouMin <show3993@naver.com>
2017-08-21 10:58:27 +0900

    Init

---
+++ app/src/main/java/com/a2017hkt15/sortaddr/MarkerController.java
@@ -0,0 +1,153 @@
+package com.a2017hkt15.sortaddr;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by YouMin on 2017-08-21.
+ */
+
+public class MarkerController {
+    // 최초 시작시 스타트 마커 여부
+    private boolean isStartExist;
+    private boolean isEndExist;
+    private int endIndex = -1;
+
+    // 현재 체크된 마커 리스트들
+    private ArrayList<TMapMarkerItem> markerList;
+
+    // 마커 중심점 위치 (하단, 중앙)
+    private float markerCenterDx = 0.5f;
+    private float markerCenterDy = 1.0f;
+
+    // 지도 포인터 및 마커 아이콘 설정
+    private TMapView tmapView;
+    private Bitmap startMarkerIcon;
+    private Bitmap passMarkerIcon;
+    private Bitmap endMarkerIcon;
+    private Bitmap[] numberMarkerIcon;
+
+    public MarkerController (TMapView tmapView, Bitmap startMarkerIcon, Bitmap passMarkerIcon, Bitmap[] markerNumberIcon, Bitmap endMarkerIcon) {
+        this.tmapView = tmapView;
+        this.isStartExist = false;
+        this.isEndExist = false;
+        this.startMarkerIcon = startMarkerIcon;
+        this.passMarkerIcon = passMarkerIcon;
+        this.numberMarkerIcon = markerNumberIcon;
+        this.endMarkerIcon = endMarkerIcon;
+        this.markerList = new ArrayList<TMapMarkerItem>();
+    }
+
+    // 위도, 경도, 마커(장소) 이름
+    public void addMarker(float latitude, float longitude, String placeName) {
+        //일반 마커
+        // 포인트 지점 인스턴스 생성 후 마커 인스턴스 생성
+        TMapPoint placePoint = new TMapPoint(latitude, longitude);
+        TMapMarkerItem placeMarker = new TMapMarkerItem();
+
+        // 마커 속성 설정
+        placeMarker.setTMapPoint(placePoint);
+        placeMarker.setID(placeName);
+        placeMarker.setName(placeName);
+        placeMarker.setVisible(TMapMarkerItem.VISIBLE);
+        placeMarker.setIcon(passMarkerIcon);
+        placeMarker.setPosition(markerCenterDx, markerCenterDy);
+        placeMarker.setCanShowCallout(true);
+        placeMarker.setAutoCalloutVisible(true);
+        placeMarker.setCalloutTitle(placeName);
+        placeMarker.setCalloutLeftImage(passMarkerIcon);
+
+        // 배열리스트 및 지도에 마커 추가
+        tmapView.addMarkerItem(placeMarker.getID(), placeMarker);
+        markerList.add(placeMarker);
+    }
+
+    public void setStartMarker(float latitude, float longitude, String placeName) {
+        // 시작 마커가 이미 있다면 제거하고 새롭게 생성
+        if ( isStartExist ) removeMarker(0);
+
+        TMapPoint placePoint = new TMapPoint(latitude, longitude);
+        TMapMarkerItem placeMarker = new TMapMarkerItem();
+
+        // 마커 속성 설정
+        placeMarker.setTMapPoint(placePoint);
+        placeMarker.setID(placeName);
+        placeMarker.setName(placeName);
+        placeMarker.setVisible(TMapMarkerItem.VISIBLE);
+        placeMarker.setIcon(startMarkerIcon);
+        placeMarker.setPosition(markerCenterDx, markerCenterDy);
+        placeMarker.setCanShowCallout(true);
+        placeMarker.setAutoCalloutVisible(true);
+        placeMarker.setCalloutTitle(placeName);
+        placeMarker.setCalloutLeftImage(startMarkerIcon);
+
+        // 배열리스트 및 지도에 마커 추가
+        tmapView.addMarkerItem(placeMarker.getID(), placeMarker);
+        markerList.add(0, placeMarker);
+        isStartExist = true;
+    }
+
+    public void setMarkerNumber(int index, int number) {
+        // 경유지 마커
+        if (number == 0) return;
+        if (isEndExist && endIndex == markerList.size() - 1) return;
+        markerList.get(index).setIcon(numberMarkerIcon[number]);
+        tmapView.removeMarkerItem(markerList.get(index).getID());
+        tmapView.addMarkerItem(markerList.get(index).getID(), markerList.get(index));
+    }
+
+    public void setEndIndex(int index) {
+        if (index == -1) {
+            this.endIndex = index;
+            this.isEndExist = false;
+            return;
+        }
+
+        if (isEndExist) {
+            markerList.get(endIndex).setIcon(passMarkerIcon);
+            tmapView.removeMarkerItem(markerList.get(endIndex).getID());
+            tmapView.addMarkerItem(markerList.get(endIndex).getID(), markerList.get(endIndex));
+        }
+
+        this.endIndex =  index;
+        this.isEndExist = true;
+        markerList.get(index).setIcon(endMarkerIcon);
+        tmapView.removeMarkerItem(markerList.get(index).getID());
+        tmapView.addMarkerItem(markerList.get(index).getID(), markerList.get(index));
+    }
+
+    public int getEndIndex() {
+        return endIndex;
+    }
+
+    // Parameter : 지울 마커의 인덱스 번호
+    public void removeMarker(int markerIndex) {
+        // 해당 번호의 마커를 맵과 배열 리스트에서 삭제.
+        tmapView.removeMarkerItem(markerList.get(markerIndex).getID());
+        markerList.remove(markerIndex);
+    }
+
+    // 모든 마커 제거 (true : 시작점 포함, false : 시작점 미포함)
+    public void removeAllMarker(boolean fromStart) {
+        int isStart;
+
+        if (fromStart) {
+            isStart = -1;
+            isStartExist = false;
+        }
+        else isStart = 0;
+
+        for (int index = markerList.size() - 1; index > isStart; index--)
+            removeMarker(index);
+    }
+
+    public ArrayList<TMapMarkerItem> getMarkerList() {
+        return markerList;
+    }
+}
---
+++ app/src/main/java/com/a2017hkt15/sortaddr/PathBasic.java
@@ -0,0 +1,119 @@
+package com.a2017hkt15.sortaddr;
+
+import android.graphics.Color;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+
+import com.skp.Tmap.TMapData;
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapPolyLine;
+import com.skp.Tmap.TMapView;
+import java.util.ArrayList;
+
+/**
+ * Created by YouMin on 2017-08-21.
+ */
+
+public class PathBasic {
+    private TMapView tmapView;
+    private TMapData tmapdata;
+    private MarkerController markerController;
+    private DistanceCalcThread distanceCalcThread;
+
+    private TMapPoint[][][] pathPoint;
+    private double[][] distanceArr;
+
+    private int[] pathRoute;
+
+    private int pathID;
+
+    public PathBasic(TMapView tmapView, MarkerController markerController) {
+        this.pathID = 0;
+        this.tmapView = tmapView;
+        this.tmapdata = new TMapData();
+        this.distanceArr = new double[10][10];
+        this.pathPoint = new TMapPoint[10][10][2];
+        this.markerController = markerController;
+    }
+
+    // 리스트에 있는 마커끼리의 거리를 계산해 거리 배열에 저장
+    public void calcDistancePath(ArrayList<TMapMarkerItem> markerList) {
+        this.removePath();
+        this.distanceCalcThread = new DistanceCalcThread(tmapdata);
+
+        for (int start = 0; start < markerList.size(); start++) {
+            for (int end = 0; end < markerList.size(); end++) {
+                if (start == end)
+                    distanceArr[start][end] = -1.0;
+                else {
+                    pathPoint[start][end][0] = markerList.get(start).getTMapPoint();
+                    pathPoint[start][end][1] = markerList.get(end).getTMapPoint();
+                }
+            }
+        }
+        distanceCalcThread.setPoint(pathPoint, markerList.size());
+        distanceCalcThread.start();
+        synchronized (distanceCalcThread) {
+            try {
+                distanceCalcThread.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        distanceArr = distanceCalcThread.getDistanceArr();
+        CalcPath calcPath = new CalcPath(0, markerController.getMarkerList().size(), distanceArr, markerController.getEndIndex());
+        this.showPath(calcPath.pathCalc());
+    }
+
+    // 완료된 경로를 지도에 표시
+    public void showPath(PathInfo pathInfo) {
+        this.setPathRoute(pathInfo.getPathRoute());
+
+        ArrayList<TMapMarkerItem> markerList = markerController.getMarkerList();
+        for(int cur = 0; cur < markerList.size() - 1; cur++) {
+            markerController.setMarkerNumber(pathRoute[cur + 1], cur + 1);
+
+            TMapPolyLine polyLine = distanceCalcThread.getPathLine()[pathRoute[cur]][pathRoute[cur + 1]];
+            polyLine.setID(pathID + "");
+            polyLine.setLineColor(Color.BLUE);
+            polyLine.setLineWidth(5);
+            tmapView.addTMapPolyLine(pathID + "Route", polyLine);
+            pathID++;
+
+            /*
+            tmapdata.findPathData(markerList.get(pathRoute[cur]).getTMapPoint(), markerList.get(pathRoute[cur+1]).getTMapPoint(), new TMapData.FindPathDataListenerCallback() {
+                @Override
+                public void onFindPathData(TMapPolyLine polyLine) {
+                    polyLine.setLineColor(Color.BLUE);
+                    polyLine.setLineWidth(5);
+                    tmapView.addTMapPolyLine(pathID + "Route", polyLine);
+                    pathID++;
+
+                    if (InputActivity.progressDialog != null) {
+                        InputActivity.progressDialog.dismiss();
+                        InputActivity.progressDialog = null;
+                    }
+                }
+            });
+            */
+        }
+    }
+
+    public int[] getPathRoute() {
+        return pathRoute;
+    }
+
+    public void setPathRoute(int[] pathRoute) {
+        this.pathRoute = pathRoute;
+    }
+
+    public void removePath() {
+        tmapView.removeAllTMapPolyLine();
+    }
+
+    public double[][] getDistanceArr() {
+        return distanceArr;
+    }
+}
+
